2f194cc9602628056c58c5583a86323d2fa0877c
MainShayne233 <shaynetremblay@gmail.com>
2019-09-11 18:41:46 -0400

    First test!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+
+fn parse_integer(value: &'static str) -> Result<(&str, i64), &str> {
+    Ok(("", 1))
+}
+
+#[test]
+fn test_parse_integer() {
+    assert_eq!(Ok(("", 1)), parse_integer("1"))
 }
